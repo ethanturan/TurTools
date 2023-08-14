819a2c552649f7e4b7d988f0502e3723829c7dc5
Ethan Turan <ethanturan@gmail.com>
2023-08-14 12:43:37 -0600

    initial

---
+++ TuranTools/BuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TuranTools;
+
+public static class BuilderExtensions
+{
+    public static IServiceCollection AddCustomOptions<T>(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        string configKey,
+        out T options
+    ) where T : class, new()
+    {
+        services.Configure<T>(configuration.GetSection(configKey));
+        options = new T();
+        configuration.Bind(configKey, options);
+
+        return services;
+    }
+}
