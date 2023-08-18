ec05f87aa1c6bde706a2c27eb24597caf8d3102d
Ethan Turan <ethanturan@gmail.com>
2023-08-18 12:37:13 -0600

    AddOptionsWithSecrets extension method on IServiceCollection

--- TurTools.SecretAbstractions/ServiceCollectionExtensions.cs
+++ TurTools.SecretAbstractions/ServiceCollectionExtensions.cs
@@ -1,34 +1,38 @@
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TurTools.SecretAbstractions;
 
 /// <summary>
-/// Extensions on WebApplicationBuilder
+/// Extensions on IServiceCollection
 /// </summary>
-public static class BuilderExtensions
+public static class ServiceCollectionExtensions
 {
     /// <summary>
     /// Registers a configuration instance which an implementation of <see cref="OptionsWithSecrets"/> will bind against
     /// Before binding, properties of <see cref="OptionsWithSecrets"/> with the <see cref="SecretKeyAttribute"/> are
     /// retrieved by by the secretRetrievalDelegate
     /// </summary>
-    /// <param name="builder">The <see cref="WebApplicationBuilder"/> to add the options to</param>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="configuration">The configuration containing the section to be registered</param>
     /// <param name="configKey">The key of the configuration section to register</param>
     /// <param name="secretRetrievalDelegate">A delegate which returns the secret corresponding to a given key (For example from Azure KeyVault)</param>
     /// <typeparam name="TOptionsWithSecrets">The options type to be configured</typeparam>
     /// <returns></returns>
-    public static WebApplicationBuilder AddOptionsWithSecrets<TOptionsWithSecrets>(
-        this WebApplicationBuilder builder,
+    public static IServiceCollection AddOptionsWithSecrets<TOptionsWithSecrets>(
+        this IServiceCollection services,
+        IConfiguration configuration, 
         string configKey,
         Func<string, Task<string?>> secretRetrievalDelegate
     ) where TOptionsWithSecrets : OptionsWithSecrets, new()
     {
-        builder.Services.AddOptionsWithSecrets<TOptionsWithSecrets>(
-            configuration: builder.Configuration,
-            configKey: configKey, 
-            secretRetrievalDelegate: async secretKey => await secretRetrievalDelegate(secretKey)
-        );
+        services.Configure<TOptionsWithSecrets>(configuration.GetSection(configKey));
+        services.Configure<TOptionsWithSecrets>(options =>
+        {
+            var task = options.PopulateSecrets(async secretKey => await secretRetrievalDelegate(secretKey));
+            task.GetAwaiter().GetResult();
+        });
 
-        return builder;
+        return services;
     }
 }
