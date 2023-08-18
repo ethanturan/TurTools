82b49510d296411c3502c91924b073fd4ee53825
Ethan Turan <ethanturan@gmail.com>
2023-08-18 14:15:26 -0600

    TurTools.Utilities initial

---
+++ TurTools.Utilities/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace TurTools.Utilities;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable) where T : struct
+    {
+        return enumerable.Where(t => t.HasValue).Select(x => x!.Value);
+    }
+    
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable) where T : class
+    {
+        return enumerable.Where(t => t is not null)!;
+    }
+}
