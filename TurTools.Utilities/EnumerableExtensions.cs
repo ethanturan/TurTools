e1fe94d1ee6c69da775c62f200abe38254b91b28
Ethan Turan <ethanturan@gmail.com>
2023-08-18 23:36:53 -0600

    add more docs

--- TurTools.Utilities/EnumerableExtensions.cs
+++ TurTools.Utilities/EnumerableExtensions.cs
@@ -16,7 +16,12 @@ public static class EnumerableExtensions
         return source.Where(t => t.HasValue).Select(x => x!.Value);
     }
     
-    /// <inheritdoc cref="WhereNotNull{T}(System.Collections.Generic.IEnumerable{System.Nullable{T}})"/>
+    /// <summary>
+    /// Removes null values and applies a null assertion to T
+    /// </summary>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> to filter null values from</param>
+    /// <typeparam name="T">The type of elements in source</typeparam>
+    /// <returns>An <see cref="IEnumerable{T}"/> result that contains no null values or references</returns>
     public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class
     {
         return source.Where(t => t is not null)!;
