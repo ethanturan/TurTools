6e9022a85ce5d54028a04a0afdd75ea210406e74
Ethan Turan <ethanturan@gmail.com>
2023-08-18 23:22:11 -0600

    add docs

--- TurTools.Utilities/EnumerableExtensions.cs
+++ TurTools.Utilities/EnumerableExtensions.cs
@@ -1,14 +1,24 @@
 ﻿namespace TurTools.Utilities;
 
+/// <summary>
+/// Extensions on IEnumerable
+/// </summary>
 public static class EnumerableExtensions
 {
-    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable) where T : struct
+    /// <summary>
+    /// Removes null values and applies a null assertion to T
+    /// </summary>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> to filter null values from</param>
+    /// <typeparam name="T">The type of elements in source</typeparam>
+    /// <returns>An <see cref="IEnumerable{T}"/> result that contains no null values or references</returns>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : struct
     {
-        return enumerable.Where(t => t.HasValue).Select(x => x!.Value);
+        return source.Where(t => t.HasValue).Select(x => x!.Value);
     }
     
-    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable) where T : class
+    /// <inheritdoc cref="WhereNotNull{T}(System.Collections.Generic.IEnumerable{System.Nullable{T}})"/>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) where T : class
     {
-        return enumerable.Where(t => t is not null)!;
+        return source.Where(t => t is not null)!;
     }
 }
